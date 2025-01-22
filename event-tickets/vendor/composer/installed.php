313befdc59b52e2e0788b1d537e90e2dd1eabbe6
Sean Murphy <sean@iamseanmurphy.com>
2025-01-22 18:19:08 +0000

    5.18.1

--- event-tickets/vendor/composer/installed.php
+++ event-tickets/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'the-events-calendar/event-tickets',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '84b10a217b4571b1fe6b69c368cb27a6970d186b',
+        'reference' => '4fb7e0b6db0066cce2cc709c37aaa877df15c1d8',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'the-events-calendar/event-tickets' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '84b10a217b4571b1fe6b69c368cb27a6970d186b',
+            'reference' => '4fb7e0b6db0066cce2cc709c37aaa877df15c1d8',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
