8d6adbc7b4f1b1b3b5a2929dac41b944481a9000
Sean Murphy <sean@iamseanmurphy.com>
2025-05-27 17:17:30 +0000

    5.23.1

--- event-tickets/vendor/composer/installed.php
+++ event-tickets/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'the-events-calendar/event-tickets',
-        'pretty_version' => 'dev-release/T25.elektra',
-        'version' => 'dev-release/T25.elektra',
-        'reference' => '92a6b51f6085a9ff611cd6d143f16f647f9b54fc',
+        'pretty_version' => 'dev-release/T25.elongatedman',
+        'version' => 'dev-release/T25.elongatedman',
+        'reference' => '23b4601e15b2fd50ba798651d8e0e013c10f1070',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'the-events-calendar/event-tickets' => array(
-            'pretty_version' => 'dev-release/T25.elektra',
-            'version' => 'dev-release/T25.elektra',
-            'reference' => '92a6b51f6085a9ff611cd6d143f16f647f9b54fc',
+            'pretty_version' => 'dev-release/T25.elongatedman',
+            'version' => 'dev-release/T25.elongatedman',
+            'reference' => '23b4601e15b2fd50ba798651d8e0e013c10f1070',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
