1bb4688b7a3ebef38f40b046328be5971e22be78
Sean Murphy <sean@iamseanmurphy.com>
2025-06-06 01:48:30 +0000

    5.24.0.1

--- event-tickets/vendor/composer/installed.php
+++ event-tickets/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'the-events-calendar/event-tickets',
-        'pretty_version' => 'dev-release/T25.enchantress',
-        'version' => 'dev-release/T25.enchantress',
-        'reference' => '240e8aef5713570e074a280a7e908c59954f76d2',
+        'pretty_version' => 'dev-release/T25.enchantress.1',
+        'version' => 'dev-release/T25.enchantress.1',
+        'reference' => '1002135a55128d732aab8be6f44b03cbc3bdb4a6',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'the-events-calendar/event-tickets' => array(
-            'pretty_version' => 'dev-release/T25.enchantress',
-            'version' => 'dev-release/T25.enchantress',
-            'reference' => '240e8aef5713570e074a280a7e908c59954f76d2',
+            'pretty_version' => 'dev-release/T25.enchantress.1',
+            'version' => 'dev-release/T25.enchantress.1',
+            'reference' => '1002135a55128d732aab8be6f44b03cbc3bdb4a6',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
