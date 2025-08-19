3e76d4e10f6c9bb3921a396d81e8fe1511601e44
Sean Murphy <sean@iamseanmurphy.com>
2025-08-19 14:17:57 +0000

    5.26.0

--- event-tickets/vendor/composer/installed.php
+++ event-tickets/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'the-events-calendar/event-tickets',
-        'pretty_version' => 'dev-release/T25.greenarrow.1',
-        'version' => 'dev-release/T25.greenarrow.1',
-        'reference' => '16b233e2ea2898391ae952fde747922fa0ea2031',
+        'pretty_version' => 'dev-release/T25.hawkeye',
+        'version' => 'dev-release/T25.hawkeye',
+        'reference' => '606235c41fee8c31a9ebe8e69853bda254028c7b',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'the-events-calendar/event-tickets' => array(
-            'pretty_version' => 'dev-release/T25.greenarrow.1',
-            'version' => 'dev-release/T25.greenarrow.1',
-            'reference' => '16b233e2ea2898391ae952fde747922fa0ea2031',
+            'pretty_version' => 'dev-release/T25.hawkeye',
+            'version' => 'dev-release/T25.hawkeye',
+            'reference' => '606235c41fee8c31a9ebe8e69853bda254028c7b',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
